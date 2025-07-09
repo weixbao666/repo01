b238dcf97332f1cc3032bb1f3c259e6a9965c6f6
weixb666 <weixb666@gmail.com>
2025-07-09 19:18:37 +0800

    remove mint function

--- index.js
+++ index.js
@@ -7,18 +7,6 @@ async function main() {
    console.log("hello world")
 }
 
-async function mint(address, pk) {
-    console.log('mint... ')
-
-    let i = 0
-    const loop=getRandomInt(6,9)
-    for(i=0;i<1;i++){
-   
-
-    }
-
-}
-
 async function swap(address, pk) {
    const url='https://localhost:1234'
    const response=await axios()
