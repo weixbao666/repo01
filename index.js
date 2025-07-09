f25d8584540972b443bafb739c3b13d37022b087
weixb666 <weixb666@gmail.com>
2025-07-09 18:28:08 +0800

    main code

---
+++ index.js
@@ -0,0 +1,37 @@
+
+import { v4 as uuidv4 } from 'uuid';
+
+
+async function main() {
+
+   
+}
+
+async function mint(address, pk) {
+    console.log('mint... ')
+
+    let i = 0
+    const loop=getRandomInt(6,9)
+    for(i=0;i<1;i++){
+   
+
+    }
+
+}
+
+async function mintBnbsDomainData(address, pk) {
+   
+
+}
+
+async function swap(address, pk) {
+   
+}
+
+function getTs(){
+    const ts=Date.now()
+    // console.log(ts)
+    return ts
+}
+// getTs()
+main().then(r => console.log(new Date()))
